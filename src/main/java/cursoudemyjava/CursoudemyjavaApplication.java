5573fef26b02a4ba208524c79ae3e8b09e65ad42
Kayo Cisz <kayocisz@gmail.com>
2024-09-08 21:38:49 -0400

    Projeto criado

---
+++ src/main/java/cursoudemyjava/CursoudemyjavaApplication.java
@@ -0,0 +1,13 @@
+package cursoudemyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoudemyjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoudemyjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cursoudemyjava/CursoudemyjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cursoudemyjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoudemyjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
